ee41707e46011c6a0b4685084b8d4aa4e91d4074
abishekk92 <bhat.abishek@gmail.com>
2013-10-13 01:45:03 +0530

    Attempting to understand the typos.
    This is an experiment towards building a statistical model

---
+++ spell_train.py
@@ -0,0 +1,29 @@
+from twython import TwythonStreamer
+from os import getenv as ENV
+from utils import *
+import dictionary
+from redis_wrapper import * 
+
+class Streamer(TwythonStreamer):
+
+    def on_success(self, data):
+        if 'text' in data:
+            tweet = extract_noise(data['text'])
+            words = normalize_words(tweet) 
+            incorrect = dictionary.check_spelling(words)
+            if incorrect is not None:
+                #for word,dmeta in zip(incorrect,dump_dmeta(incorrect)):
+                #    #if is_it_english(word):
+                #    #    dictionary.add_words([words])
+                #    print word,dictionary.confidence_score(dmeta)
+                error_counts(incorrect)
+
+    def on_error(self,status_code,data):
+        print "Error:%s" % status_code
+
+
+
+stream = Streamer(ENV("twitter_cons_key"), ENV("twitter_cons_secret"),
+        ENV("twitter_auth_token"), ENV("twitter_auth_secret"))
+
+stream.statuses.filter(track='#help',language='en')
